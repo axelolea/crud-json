917d8b5ab28ee33a59947d5535b6858d793e0e34
axelolea <axelcedilloolea@gmail.com>
2023-09-07 22:15:24 -0600

    First Commit o.o

---
+++ src/main/java/com/axelolea/crudjson/CrudJsonApplication.java
@@ -0,0 +1,13 @@
+package com.axelolea.crudjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axelolea/crudjson/CrudJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelolea.crudjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
