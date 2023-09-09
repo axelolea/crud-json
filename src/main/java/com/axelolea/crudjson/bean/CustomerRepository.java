ac68012d74069b793a111d10837b15e28b0cbd3a
axelolea <axelcedilloolea@gmail.com>
2023-09-09 15:05:48 -0600

    Use repo with driver

--- src/main/java/com/axelolea/crudjson/bean/CustomerRepository.java
+++ src/main/java/com/axelolea/crudjson/bean/CustomerRepository.java
@@ -1,34 +1,71 @@
 package com.axelolea.crudjson.bean;
 
 import com.axelolea.crudjson.dto.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class CustomerRepository implements CrudRepository<CustomerDto, Long> {
+public class CustomerRepository implements CrudRepository {
+
+    JsonDriver driver;
+    @Autowired
+    public CustomerRepository(JsonDriver jsonDriver) {
+        this.driver = jsonDriver;
+    }
 
     @Override
-    public CustomerDto save(CustomerDto i) {
-        return null;
+    public CustomerDto save(CustomerDto customer) {
+
+        CustomerJson data = driver.getJsonData();
+        long offset = data.getSerialOffset();
+
+        customer.setId(offset);
+
+        data.getData().add(customer);
+        data.autoincrement();
+
+        driver.setJsonData(data);
+
+        return customer;
     }
 
     @Override
     public Optional<CustomerDto> findById(Long id) {
-        return null;
+
+        return driver.getJsonData().getData().stream()
+                .filter(o -> o.getId() == id)
+                .findFirst();
+
     }
 
     @Override
     public List<CustomerDto> findAll() {
-        return null;
+        return driver.getJsonData().getData();
     }
 
     @Override
     public void delete(Long id) {
+        CustomerJson data = driver.getJsonData();
 
+        List<CustomerDto> customerList = data.getData().stream()
+                .filter(obj -> obj.getId() != id)
+                .toList();
+
+        if(customerList.isEmpty()) {
+            data.setData(new ArrayList<>());
+        } else {
+            data.setData(customerList);
+        }
+
+        driver.setJsonData(data);
     }
 
     @Override
     public CustomerDto update(Long id, CustomerDto customerDto) {
         return null;
     }
+
 }
